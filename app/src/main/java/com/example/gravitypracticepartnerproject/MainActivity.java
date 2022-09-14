dfe62bc557588ca4ac83e322f307e9b781bc5524
markariagno22 <Mark.ariagno14@gmail.com>
2022-09-13 22:34:51 -0500

    Finalized design of app

--- app/src/main/java/com/example/gravitypracticepartnerproject/MainActivity.java
+++ app/src/main/java/com/example/gravitypracticepartnerproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gravitypracticepartnerproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout currentLayout = findViewById(R.id.frameLayout);
+
+        currentLayout.setBackgroundColor(Color.WHITE);
     }
 }
